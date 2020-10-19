bd9c9362f51bdef261cedd991de9c56fa36b776f
Spaksoo <73102653+Spaksoo@users.noreply.github.com>
2020-10-19 12:05:44 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,19 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/ponchik.png');
     }
 }
+let myButton = document.querySelector('button')
+let myHeading = document.querySelector('h1')
+function setUserName(){
+    let myName = promt('Please enter your name');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Chrome is cool,' + myName;
+}
+if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Chrome is cool, ' + storedName;
+  }
+  myButton.onclick = function() {
+    setUserName();
+  }
